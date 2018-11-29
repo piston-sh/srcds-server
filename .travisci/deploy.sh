079469b653f8d2fcac544d5d6566f83ab740da59
Denis Craig <dcraig@thousandeyes.com>
2018-11-29 13:43:37 +0000

    Add additional distro support

--- .travisci/deploy.sh
+++ .travisci/deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-docker push sourceforts/dedicated-server:debian-$TRAVIS_BUILD_NUMBER
-docker push sourceforts/dedicated-server:debian
+
+# ubuntu-trusty
+docker push sourceforts/dedicated-server:ubuntu-trusty-$TRAVIS_BUILD_NUMBER
+docker push sourceforts/dedicated-server:ubuntu-trusty
+docker push sourceforts/dedicated-server:ubuntu
 docker push sourceforts/dedicated-server:latest
+
+# ubuntu-xenial
+docker push sourceforts/dedicated-server:ubuntu-xenial-$TRAVIS_BUILD_NUMBER
+docker push sourceforts/dedicated-server:ubuntu-xenial
+
+# debian-stretch
+docker push sourceforts/dedicated-server:debian-stretch-$TRAVIS_BUILD_NUMBER
+docker push sourceforts/dedicated-server:debian-stretch
+docker push sourceforts/dedicated-server:debian
