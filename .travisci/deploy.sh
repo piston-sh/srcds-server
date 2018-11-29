4790d3cb3ce00df6d3bc8ee8d85be3ea4add2ecd
Denis Craig <dcraig@thousandeyes.com>
2018-11-29 21:21:32 +0000

    Remove a bunch of dependency installations as we're using 32bit ubuntu now

--- .travisci/deploy.sh
+++ .travisci/deploy.sh
@@ -3,12 +3,12 @@
 # ubuntu-trusty
 docker push sourceforts/dedicated-server:ubuntu-trusty-$TRAVIS_BUILD_NUMBER
 docker push sourceforts/dedicated-server:ubuntu-trusty
-docker push sourceforts/dedicated-server:ubuntu
-docker push sourceforts/dedicated-server:latest
 
 # ubuntu-xenial
 docker push sourceforts/dedicated-server:ubuntu-xenial-$TRAVIS_BUILD_NUMBER
 docker push sourceforts/dedicated-server:ubuntu-xenial
+docker push sourceforts/dedicated-server:ubuntu
+docker push sourceforts/dedicated-server:latest
 
 # debian-stretch
 docker push sourceforts/dedicated-server:debian-stretch-$TRAVIS_BUILD_NUMBER
